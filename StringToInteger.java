0c4a046e6bc8040b4bd59a049b8cbf56b0ca3f10
AtulA121 <54090342+AtulA121@users.noreply.github.com>
2020-02-11 19:13:10 +0530

    Add files via upload

--- StringToInteger.java
+++ StringToInteger.java
@@ -1,10 +1,11 @@
 public class StringToInteger {
     public static void main(String[] args){
-        String str="   -231";
+//        String str="-91283472332";
+        String str="+1";
         System.out.println(getResult(str));
     }
     private static int getResult(String str){
-        int result=0;
+        long result=0;
         boolean sign=false;
         for(int i=0;i<str.length();i++){
             char ch=str.charAt(i);
@@ -12,10 +13,26 @@ public class StringToInteger {
                 sign=true;
             }else if(ch>=48 && ch<=57){
                 result=result*10+Integer.parseInt(ch+"");
+                if(result>Integer.MAX_VALUE || result<Integer.MIN_VALUE) {
+                    return returnResult(sign);
+                }
+            }else if (ch=='+'){
+                sign=false;
+            }else if(ch!=' '){
+                break;
             }
         }
         if (sign){
             result=-result;
+        }
+        return (int)result;
+    }
+    private static int returnResult(boolean sign){
+        int result=0;
+        if(sign){
+            result=Integer.MIN_VALUE;
+        }else{
+            result=Integer.MAX_VALUE;
         }
         return result;
     }
